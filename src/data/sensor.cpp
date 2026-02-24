7522a36ff5dae2bdf3d0c432cba7bb048bdf8578
Sharan Sapkota <sapkotarambbo@gmail.com>
2026-02-24 06:18:52 +0200

    Update mock sensorData

--- src/data/sensor.cpp
+++ src/data/sensor.cpp
@@ -20,11 +20,17 @@ SensorData Sensor::getRealData() {
 SensorData Sensor::getMockData() {
     SensorData data;
     data.sensorId = this->sensorId;
-    data.rawADC = random(100, 500);
-    data.hydrogenPpm = (data.rawADC / 4095.0f) * 1000.0f;
-    data.temperature = 24.5f + random(-10, 10) / 10.0f;
-    data.humidity = 55.0f + random(-50, 50) / 10.0f;
-    data.timestamp = millis();
+    bool isLeak = random(0, 10) > 7;
+
+    if (isLeak) {
+        data.hydrogenPpm = random(5000, 10000); 
+    } else {
+        data.hydrogenPpm = random(0, 500);
+    }
+    data.rawADC      = (data.hydrogenPpm / (float)SENSOR_MAX_PPM) * 4095;
+    data.temperature = 24.5f + random(-20, 20) / 10.0f;
+    data.humidity    = 55.0f + random(-50, 50) / 10.0f;
+    data.timestamp   = millis();
     
     return data;
 }
