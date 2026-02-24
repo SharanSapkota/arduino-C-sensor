30a568af06bc62835dc4e02c41cf143a906bcc44
Sharan Sapkota <sapkotarambbo@gmail.com>
2026-02-24 12:37:49 +0200

    Update the env to pico w compatible

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 #include "sensor/sensor.h"
 #include "mqtt/mqtt.h"
 
-#define SENSOR_ID       1 
-#define ANALOG_PIN      34     
+#define ANALOG_PIN  26     
+#define SENSOR_ID  1 //We need to update the id according to the sensor
 #define USE_MOCK_DATA   true
 
-const char* WIFI_SSID     = "your_SSID";
+const char* WIFI_SSID = "your_SSID";
 const char* WIFI_PASSWORD = "your_PASSWORD";
-const char* MQTT_BROKER   = "192.168.1.100";  
-const int   MQTT_PORT     = 1883;
+const char* MQTT_BROKER = "192.168.1.100";  
+const int   MQTT_PORT = 1883;
 
 Internet internet(WIFI_SSID, WIFI_PASSWORD);
 Sensor sensor(SENSOR_ID, ANALOG_PIN);
